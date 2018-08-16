cc30e37b3a145ba7beda404f8239111bb2ed04b8
braj011 <bhagyaraj01@gmail.com>
2018-08-16 23:08:05 +0000

    automatically backed up by learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -5,17 +5,7 @@ def levitation_quiz
 	    answer = gets.chomp
 	    break if answer = "Wingardium Leviosa"
 	  end 
-	  puts 
+	  puts "You passed the quiz!" 
 end
+levitation_quiz
 
-
-def annoying
-    loop do 
-        puts "What's up, Doc?"
-        answer = gets.chomp
-        break if answer == "STOP"
-    end
-    puts "Okay, okay, jeez. I'll stop. Sorry."
-end
- 
-annoying
